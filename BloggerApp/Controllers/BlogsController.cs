9f7346acd6fe4c50c949065760f1fc96e59142e2
Neha Singh <neha.singh@cybergindia.local>
2017-12-25 21:13:46 +0530

    Blog related api are complete!

--- BloggerApp/Controllers/BlogsController.cs
+++ BloggerApp/Controllers/BlogsController.cs
@@ -10,12 +10,131 @@ namespace BloggerApp.Controllers
 {
     public class BlogsController : ApiController
     {
-            public HttpResponseMessage Get(string gender = "All")
+        /// <summary>
+        /// Api to Get all Blogs created ! 
+        /// </summary>
+        /// <returns></returns>
+        public HttpResponseMessage Get()
+        {
+            using (BloggerAppDBEntities entities = new BloggerAppDBEntities())
+            {
+                var entity = entities.Blog_Detail.ToList();
+                if(entity  != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entities.Blog_Detail.ToList());
+                }
+               else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "No Blog found ! ");
+                }
+            }
+        }
+        /// <summary>
+        /// To Get Blogs created by  specific Blogger 
+        /// </summary>
+        /// <param name="id"> it specifies the BID of Blogger</param>
+        /// <returns></returns>
+        public HttpResponseMessage Get(int id)
+        {
+            using (BloggerAppDBEntities entities = new BloggerAppDBEntities())
+            {
+                var blogs = entities.Blog_Detail.ToList();   
+               var blogsById = blogs.Where(e => e.UID == id).ToList();
+
+                if (blogsById != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, blogsById);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "No blog found Present for Blogger having UID " + id + " not found !");
+                }
+
+            }
+        }
+        /// <summary>
+        /// To create Blog
+        /// </summary>
+        /// <param name="blog"></param>
+        /// <returns></returns>
+        public HttpResponseMessage Post([FromBody] Blog_Detail blog)
+        {
+            try
             {
                 using (BloggerAppDBEntities entities = new BloggerAppDBEntities())
                 {
-                       return Request.CreateResponse(HttpStatusCode.OK, entities.User_Details.ToList());
-                }
+                    blog.DateOfUpdation = DateTime.Now;
+                    entities.Blog_Detail.Add(blog);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, blog);
+                    message.Headers.Location = new Uri(Request.RequestUri + blog.Title.ToString());
+                    return message;
                 }
             }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+
+        /// <summary>
+        /// To delete blog based on its id i.e. BID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public HttpResponseMessage Delete(int id)
+        {
+
+            using (BloggerAppDBEntities entity = new BloggerAppDBEntities())
+            {
+                var blogToDelete = entity.Blog_Detail.FirstOrDefault(e => e.BID == id);
+                if (blogToDelete != null)
+                {
+                    entity.Blog_Detail.Remove(blogToDelete);
+                    entity.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "Blog with BID = " + id + " Not found ! ");
+                }
+            }
+        }
+        /// <summary>
+        /// To update Blog
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="blog"></param>
+        /// <returns></returns>
+        public HttpResponseMessage put(int id, [FromBody] Blog_Detail blog)
+        {
+            try
+            {
+                using (BloggerAppDBEntities entity = new BloggerAppDBEntities())
+                {
+                    var blogToEdit = entity.Blog_Detail.FirstOrDefault(e => e.BID == id);
+                    if (blogToEdit != null)
+                    {
+                        blogToEdit.Title = blog.Title;
+                        blogToEdit.Blog_Content = blog.Blog_Content;
+                        blogToEdit.DateOfUpdation = DateTime.Now;
+                        entity.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, blogToEdit);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Blog with BID = " + id + " Not found ! ");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
     }
+}
