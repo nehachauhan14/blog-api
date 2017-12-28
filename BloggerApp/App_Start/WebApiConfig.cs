ee35933b9dcdda7ae66656ded2421c27abce86fa
Neha Singh <neha.singh@cybergindia.local>
2017-12-28 11:19:01 +0530

    Need to implement auth

--- BloggerApp/App_Start/WebApiConfig.cs
+++ BloggerApp/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace BloggerApp
 {
@@ -27,6 +28,9 @@ namespace BloggerApp
 
             config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "GET, POST");
+            config.EnableCors(cors);
 
             //var json = config.Formatters.JsonFormatter;
             //json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
