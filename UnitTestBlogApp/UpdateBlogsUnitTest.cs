539669568db4839d72ccfeff2f8f4c25d4b505c7
Neha Singh <neha.singh@cybergindia.local>
2018-01-30 11:00:39 +0530

    Unit Test for Update functionality are complete.

--- UnitTestBlogApp/UpdateBlogsUnitTest.cs
+++ UnitTestBlogApp/UpdateBlogsUnitTest.cs
@@ -9,19 +9,60 @@ namespace UnitTestBlogApp
     public class UpdateBlogsUnitTest : BaseUnitTest
     {
         BlogsController controller = new BlogsController();
+        int correctBid = 1009;
+        int incorrectBid = 0;
 
         [TestMethod]
-        public void Update_with_correct_data()
+        public void Update_CorrectBID_ShouldUpdate()
         {
-            Blog_Detail response = controller.UpdateBlogById(1009, GenerateRandomBlog());
-            Assert.AreSame(GenerateRandomBlog(), response);
+            Blog_Detail RandomGeneratedBlog = GenerateRandomBlog();
+
+            Blog_Detail response = controller.UpdateBlogById(correctBid , RandomGeneratedBlog);
+            bool BlogsAreEqual = response.Title == RandomGeneratedBlog.Title && response.Blog_Content == RandomGeneratedBlog.Blog_Content;
+
+            Assert.IsTrue(BlogsAreEqual, "Blog having correct BID has been updated successfully! ");
         }
+
+        [TestMethod]
+        public void Update_IncorrectBID_shouldReturnNull()
+        {
+            Blog_Detail response = controller.UpdateBlogById(incorrectBid, GenerateRandomBlog());
+            Assert.AreEqual(null, response.Title);
+            Assert.AreEqual(null, response.Blog_Content);
+        }
+
+        [TestMethod]
+        public void Update_TitleIsValid_shouldupdate()
+        {
+            var updatedBlog = GenerateRandomBlog();
+            var updatedTitle = updatedBlog.Title;
+            var updatedContent = updatedBlog.Blog_Content;
+
+            Blog_Detail response = controller.UpdateBlogById(correctBid, updatedBlog);
+            Assert.AreEqual(updatedTitle, response.Title);
+          
+        }
+
+        [TestMethod]
+        public void Update_BlogContentIsValid_ShouldUpdate()
+        {
+            var updatedBlog = GenerateRandomBlog();
+            var updatedTitle = updatedBlog.Title;
+            var updatedContent = updatedBlog.Blog_Content;
+       
+            Blog_Detail response = controller.UpdateBlogById(correctBid, updatedBlog);
+            Assert.AreEqual(updatedContent, response.Blog_Content);
+        }
+
+
+
 
         #region Model Creator
         private Blog_Detail GenerateRandomBlog()
         {
             return new Blog_Detail
             {
+               
                 Title = GenerateRandomString(10),
                 Blog_Content = GenerateRandomString(100),
                 DateOfUpdation = DateTime.Now
